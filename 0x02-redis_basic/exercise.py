2765fa4e9792eca4d5b6e7c7470e31b40b6fdf38
abraham <wole1702@gmail.com>
2024-10-23 22:28:30 +0100

    exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+writing strings to redis
+ """
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+    """ redis cache"""
+    def __init__(self):
+        """ stores an instance of reddis cient
+            as a private variable name _redis
+            and flush the instance using flush
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[int, float, bytes, str]) -> str:
+        """ create a unique key and stores the data"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
